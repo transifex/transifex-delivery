56de17e3f3050bc4da1b60e9f0455230b596bb80
Nikos Vasileiou <nikosv@transifex.com>
2022-03-22 14:50:31 +0200

    Cache analytic results

--- src/routes/analytics.js
+++ src/routes/analytics.js
@@ -69,42 +69,56 @@ router.get('/',
     const { total } = response.meta;
 
     for (let i = 0; i <= intervals; i += 1) {
-      const keyDay = dateSince.add(i, 'day').format('YYYY-MM-DD');
+      const analyticsDate = dateSince.add(i, 'day');
+      const keyDay = analyticsDate.format('YYYY-MM-DD');
 
       const registryKey = `analytics:${req.token.project_token}:${keyDay}`;
-      const entry = {
-        languages: {},
-        sdks: {},
-        date: keyDay,
-      };
 
-      await Promise.all([
-        // languages
-        (async () => {
-          const langCodes = await registry.listSet(`${registryKey}:lang`);
-          await Promise.all(_.map(langCodes, (lang) => (async () => {
-            const count = await registry.get(`${registryKey}:lang:${lang}`);
-            if (count > 0) {
-              entry.languages[lang] = count;
-              total.languages[lang] = total.languages[lang] || 0;
-              total.languages[lang] += count;
-            }
-          })()));
-        })(),
-        // SDKs
-        (async () => {
-          const sdkVersions = await registry.listSet(`${registryKey}:sdk`);
-          await Promise.all(_.map(sdkVersions, (sdk) => (async () => {
-            const count = await registry.get(`${registryKey}:sdk:${sdk}`);
-            if (count > 0) {
-              entry.sdks[sdk] = count;
-              total.sdks[sdk] = total.sdks[sdk] || 0;
-              total.sdks[sdk] += count;
-            }
-          })()));
-        })(),
-      ]);
-      response.data.push(entry);
+      // check for aggregated results
+      const aggrKey = `${registry}:aggr`;
+      const aggrEntry = await registry.get(aggrKey);
+      if (aggrEntry) {
+        response.data.push(aggrEntry);
+      } else {
+        const entry = {
+          languages: {},
+          sdks: {},
+          date: keyDay,
+        };
+
+        await Promise.all([
+          // languages
+          (async () => {
+            const langCodes = await registry.listSet(`${registryKey}:lang`);
+            await Promise.all(_.map(langCodes, (lang) => (async () => {
+              const count = await registry.get(`${registryKey}:lang:${lang}`);
+              if (count > 0) {
+                entry.languages[lang] = count;
+                total.languages[lang] = total.languages[lang] || 0;
+                total.languages[lang] += count;
+              }
+            })()));
+          })(),
+          // SDKs
+          (async () => {
+            const sdkVersions = await registry.listSet(`${registryKey}:sdk`);
+            await Promise.all(_.map(sdkVersions, (sdk) => (async () => {
+              const count = await registry.get(`${registryKey}:sdk:${sdk}`);
+              if (count > 0) {
+                entry.sdks[sdk] = count;
+                total.sdks[sdk] = total.sdks[sdk] || 0;
+                total.sdks[sdk] += count;
+              }
+            })()));
+          })(),
+        ]);
+        response.data.push(entry);
+
+        // store past results in registry for future look ups
+        if (dayjs().diff(analyticsDate, 'days') > 0) {
+          await registry.set(aggrKey, entry, analyticsRetentionDays);
+        }
+      }
     }
 
     res.json(response);
